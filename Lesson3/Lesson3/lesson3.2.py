316fec33f0de6b9d97b1dd4e2d114afa0a31ea3a
Elpatevskiy <84735714+Alexandr111113@users.noreply.github.com>
2021-08-19 11:33:38 +0300

    Add files via upload

---
+++ Lesson3/Lesson3/lesson3.2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+
+my_func(name= 'Иван', surname='Иванов', byear=1990, city='Spb', email='email@email.ru', phone='8911')
